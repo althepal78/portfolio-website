8534582669d5fdedd0927d4380969fcd968ef3f4
althepal78 <althepal78@gmail.com>
2024-10-03 16:27:44 -0400

    changed the h1 to clamp and the variable to have a max of 4 rem and a min of 1 rem change classes to work directly with the h1 vs saying hello and different names to the same thing

--- src/js/main.js
+++ src/js/main.js
@@ -6,20 +6,20 @@ hireMeButton.addEventListener('click', () => {
   hireMeForm.style.display = 'block';
 });
 
-hireMeFormInner.addEventListener('submit', (e) => {
-  e.preventDefault();
-  const formData = new FormData(hireMeFormInner);
+// hireMeFormInner.addEventListener('submit', (e) => {
+//   e.preventDefault();
+//   const formData = new FormData(hireMeFormInner);
 
-  // Send the data to your server for email processing
-  fetch('email-script.php', {
-    method: 'POST',
-    body: formData,
-  }).then((response) => {
-    if (response.ok) {
-      alert('Your message has been sent.');
-      hireMeForm.style.display = 'none';
-    } else {
-      alert('Error sending the message. Please try again.');
-    }
-  });
-});
+//   // Send the data to your server for email processing
+//   fetch('email-script.php', {
+//     method: 'POST',
+//     body: formData,
+//   }).then((response) => {
+//     if (response.ok) {
+//       alert('Your message has been sent.');
+//       hireMeForm.style.display = 'none';
+//     } else {
+//       alert('Error sending the message. Please try again.');
+//     }
+//   });
+// });
